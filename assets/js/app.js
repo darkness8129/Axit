05a306129424d54570721256a4a049767ea27650
darkness8129 <yaroslaw460@gmail.com>
2020-11-16 16:27:39 +0200

    fixed slides

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,38 +1,38 @@
 "use strict";
-window.onload = function(){
-	document.getElementById("defaultOpen").click();
+window.onload = function () {
+    document.getElementById("defaultOpen").click();
 }
 
 function open__item(event, item) {
-	var i, slider__item, slid;
-	slider__item = document.getElementsByClassName('slider__item');
-	slid = document.getElementsByClassName('slid');
+    var i, slider__item, slid;
+    slider__item = document.getElementsByClassName('slider__item');
+    slid = document.getElementsByClassName('slide');
 
-	for (i = 0; i < slider__item.length; i++) {
-		slider__item[i].style.display = "none";
-		console.log(slider__item[i]);
-	}
-	for (i = 0; i < slid.length; i++) {
-    	slid[i].className = slid[i].className.replace(" active", "");
-  	}
+    for (i = 0; i < slider__item.length; i++) {
+        slider__item[i].style.display = "none";
+        console.log(slider__item[i]);
+    }
+    for (i = 0; i < slid.length; i++) {
+        slid[i].className = slid[i].className.replace(" active", "");
+    }
 
-	document.getElementById(item).style.display = "grid";
-	event.currentTarget.className += " active";
-
-}
-
-function show__top(){
-	var top__card = document.getElementById("top__card");
-	var card__header2 = document.getElementById("card__header2");
-	top__card.style.display = "block";
-	card__header2.style.paddingBottom=0;
-	card__header2.style.borderBottom="none";
+    document.getElementById(item).style.display = "grid";
+    event.currentTarget.className += " active";
 
 }
-function hide__top(){
-		top__card.style.display = "none";
-		card__header2.style.paddingBottom=10+"px";
-		card__header2.style.borderBottom="5px solid #ff8b38";
+
+function show__top() {
+    var top__card = document.getElementById("top__card");
+    var card__header2 = document.getElementById("card__header2");
+    top__card.style.display = "block";
+    card__header2.style.paddingBottom = 0;
+    card__header2.style.borderBottom = "none";
+
+}
+function hide__top() {
+    top__card.style.display = "none";
+    card__header2.style.paddingBottom = 10 + "px";
+    card__header2.style.borderBottom = "5px solid #ff8b38";
 
 }
 
